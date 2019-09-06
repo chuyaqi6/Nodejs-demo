a8ff8e0c5127f5258ffbe2dc3a4d5ca80b412910
chuyaqi6 <2484917266@qq.com>
2019-09-06 19:23:51 +0800

    add 01/02/03/04/template

---
+++ 03-console/01-format.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+
+const log = console.log;
+
+var cyq = {
+    'Name':'褚雅琦',
+      'QQ':'2484917266',
+        'Age':21
+
+};
+
+log('Name:%s',cyq.Name);
+log('Age:%d',cyq.Age);
+log('Chuyaqi Info:%J',cyq);
+
+log('Name:',cyq.Name);
+log(`Age is ${cyq.Age}`);
---
+++ 04-process/05-my-kill.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+
+var pid = process.argv[2];
+    sng = process.argv[3];
+    process.kill(pid,sgn);
