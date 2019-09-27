b33393c507e1d965a9e1131ace7787f67f39ef5e
chuyaqi6 <2484917266@qq.com>
2019-09-27 17:02:15 +0800

    add 16

---
+++ 16-http-client/01-my-curl.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+const http = require('http'),
+      addr = process.argv[2] || 'http://sample.wangding.in/web/one-div.html';
+
+http.get(addr,function(res){
+  //打印开始
+  console.log(`HTTP/${res.httpVersion} ${res.statusCode} ${res.statusMessage}`);
+  //打印响应头
+  console.log(res.headers);
+  console.log('');
+  //打印响应体
+  res.pipe(process.stdout);
+});
